2447fe5ce881659c8476c6d760e5c31741a5b4e8
CKalitin <Christopher.Kalitin@gmail.com>
2023-04-13 11:47:48 -0700

    Fixed Structure Health Bar

--- Tiny-Troops-Client/Assets/Scripts/GUI/Displays/StructureHealthDisplay.cs
+++ Tiny-Troops-Client/Assets/Scripts/GUI/Displays/StructureHealthDisplay.cs
@@ -5,8 +5,23 @@ using UnityEngine.UI;
 
 public class StructureHealthDisplay : MonoBehaviour {
     [SerializeField] private Image healthBar;
-    [Space]
-    [SerializeField] private Health health;
+    
+    private Health health;
+    
+    private void Start() {
+        Transform t = transform.parent;
+        while (true) {
+            if (t.GetComponent<Health>()) {
+                // If tile script found
+                health = t.GetComponent<Health>();
+                break;
+            } else {
+                // If we're at the top of the heirarchy, break out of the loop.
+                if (t.parent == null) break;
+                t = t.parent;
+            }
+        }
+    }
 
     private void Update() {
         healthBar.fillAmount = health.CurrentHealth / health.MaxHealth;
