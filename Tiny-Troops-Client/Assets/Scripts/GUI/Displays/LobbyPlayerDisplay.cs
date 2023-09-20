dda1dfaf9f95de901890970591867f0a0f9f0a32
CKalitin <Christopher.Kalitin@gmail.com>
2023-09-19 19:10:21 -0700

    Lobby Ready Spacing Fix
    The username "MMMMMMMMMMMMMMMM" used to break everything. Now it doesn't.

--- Tiny-Troops-Client/Assets/Scripts/GUI/Displays/LobbyPlayerDisplay.cs
+++ Tiny-Troops-Client/Assets/Scripts/GUI/Displays/LobbyPlayerDisplay.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -10,11 +10,20 @@ public class LobbyPlayerDisplay : MonoBehaviour {
     [Tooltip("If the player's username skips to the next line, the next ready position is used.")]
     [SerializeField] Transform[] togglableReadyPositions;
 
+    private float defaultPreferredHeight;
+
     public TextMeshProUGUI UsernameText { get => usernameText; set => usernameText = value; }
     public GameObject TogglableReady { get => togglableReady; set => togglableReady = value; }
 
+    private void Awake() {
+        usernameText.text = "‎";
+        defaultPreferredHeight = usernameText.preferredHeight;
+    }
+
     private void Start() {
-        togglableReady.transform.parent.position = togglableReadyPositions[0].position;
-        if (usernameText.text.Length > 12) togglableReady.transform.parent.position = togglableReadyPositions[1].position;
+        Debug.Log(usernameText.preferredHeight + ", " + defaultPreferredHeight + ", " + Mathf.FloorToInt(usernameText.preferredHeight / (defaultPreferredHeight - 1) - 1));
+        int spacingLines = Mathf.FloorToInt(usernameText.preferredHeight / (defaultPreferredHeight - 1) - 1);
+
+        togglableReady.transform.parent.position = togglableReadyPositions[spacingLines].position;
     }
 }
